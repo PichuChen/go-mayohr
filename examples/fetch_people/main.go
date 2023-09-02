509380137d782087896cf18e44053f97fafa09c5
Pichu <pichu.baby@gmail.com>
2023-09-02 15:47:19 +0800

    add search people

---
+++ examples/fetch_people/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/pichuchen/mayohr"
+)
+
+func main() {
+	username := os.Getenv("MAYOHR_USERNAME") // Change this to your username
+	password := os.Getenv("MAYOHR_PASSWORD") // Change this to your password
+
+	c := mayohr.NewClient(username, password)
+	err := c.Login()
+	if err != nil {
+		fmt.Printf("failed to login: %v", err)
+		return
+	}
+	fmt.Printf("ID Token: %v", c.IDToken)
+	totalPeopleList, err := c.FetchAllPeople()
+	if err != nil {
+		fmt.Printf("failed to get people: %v", err)
+		return
+	}
+
+	fmt.Printf("People: len=%d %+v", len(totalPeopleList), totalPeopleList[:3])
+
+}
